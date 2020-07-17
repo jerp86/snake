0fa1c00c343a66ee697fc4a2ba843ec3526585de
Jose Eduardo <jerp4@hotmail.com>
2020-07-17 14:18:43 -0300

    Add end in game and alert

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById("snake");
 const context = canvas.getContext("2d");
 const box = 32;
 const snake = [];
+let count = 0;
 let direction = 'right';
 
 snake[0] = {
@@ -51,6 +52,13 @@ const startGame = () => {
   if (snake[0].y > 15 * box && direction === 'down') snake[0].y = 0;
   if (snake[0].y < 0 && direction === 'up') snake[0].y = 16 * box;
 
+  for (let i = 1; i < snake.length; i++) {
+    if (snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
+      clearInterval(game);
+      alert(`😲 Game over 😭\n You eat ${count} foods`)
+    }
+  }
+
   createBG();
   createLittleSnake();
   drawFood();
@@ -66,7 +74,10 @@ const startGame = () => {
   if (snakeX !== food.x || snakeY !== food.y) {
     snake.pop();
   } else {
+    console.log('Antes de comer:', count);
+    count++;
     food = position()
+    console.log('Depois de comer:', count);
   }
 
   const newHead = {
