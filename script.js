d4674fe98252f9f222876fb4f69e345017813e16
Jose Eduardo <jerp4@hotmail.com>
2020-07-17 12:55:25 -0300

    Define snake motions

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById("snake");
 const context = canvas.getContext("2d");
 const box = 32;
 const snake = [];
+const direction = 'right';
 
 snake[0] = {
   x: 8 * box,
@@ -20,5 +21,26 @@ const createLittleSnake = () => {
   }
 };
 
-createBG();
-createLittleSnake();
+const startGame = () => {
+  createBG();
+  createLittleSnake();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if (direction === 'right') snakeX += box;
+  if (direction === 'left') snakeX -= box;
+  if (direction === 'up') snakeY -= box;
+  if (direction === 'down') snakeX += box;
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  };
+
+  snake.unshift(newHead);
+};
+
+const game = setInterval(startGame, 100);
